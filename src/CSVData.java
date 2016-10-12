6853ec539889cc727d1301a5a3f29acd02a5365a
Anika Vichare <anikavichare@Anikas-MacBook-Pro.local>
2016-10-12 11:32:18 -0700

    Add final method stubs to CSV Data.java

--- src/CSVData.java
+++ src/CSVData.java
@@ -5,71 +5,106 @@
  */
 public class CSVData {
 
-	private double [][] data;//if static, we cannot make data objects with different data variables
-	private String [] columnNames;
-	
-	public static CSVData readCSVFile(String filename, int numLinesToIgnore, String[]columnNames){
-		
+	private double[][] data;// if static, we cannot make data objects with
+							// different data variables
+	private String[] columnNames;
+
+	// number of rows: data.length
+
+	public static CSVData readCSVFile(String filename, int numLinesToIgnore, String[] columnNames) {
+
 		return null;
-		
+
 	}
+
 	/***
-	 * Returns a new CSV Data object for a file ignoring lines at the top
-	 * It uses the first row as column names. All other data is stored as doubles.
-	 * @param filename the file to read
-	 * @param numLinesToIgnore number of lines at the top to ignore
+	 * Returns a new CSV Data object for a file ignoring lines at the top It
+	 * uses the first row as column names. All other data is stored as doubles.
+	 * 
+	 * @param filename
+	 *            the file to read
+	 * @param numLinesToIgnore
+	 *            number of lines at the top to ignore
 	 * @return a CSV data object for that file
 	 */
-	public static CSVData readCSVFile(String filename, int numLinesToIgnore){
+	public static CSVData readCSVFile(String filename, int numLinesToIgnore) {
 		return null;
-		
-		
+
 	}
+
 	/***
 	 * returns a row
-	 * @param rowIndex index of the row to return
+	 * 
+	 * @param rowIndex
+	 *            index of the row to return
 	 * @return the row we're returning
 	 */
-	public double[] getIndividualRow(int rowIndex){
+	public double[] getIndividualRow(int rowIndex) {
 		return null;
 	}
-	
-	public double[] getIndividualColumn(int columnIndex){
+
+	/***
+	 * returns an individual column
+	 * 
+	 * @param columnIndex
+	 *            index of column being returned
+	 * @return the column we're returning
+	 */
+	public double[] getIndividualColumn(int columnIndex) {
 		return null;
 	}
-	
-	public double[] getIndividualColumn(String name){
+
+	public double[] getIndividualColumn(String name) {
 		return null;
 	}
-	
-	
-	public double[][] getRows(int startIndex, int endIndex){
+
+	/***
+	 * returns multiple rows
+	 * 
+	 * @param startIndex
+	 *            first index of rows
+	 * @param endIndex
+	 *            last index of rows being returned
+	 * @return the rows that we're returning
+	 */
+	public double[][] getRows(int startIndex, int endIndex) {
 		return null;
 	}
-	
-	public double[][] getRows(int [] rowIndexes){
+
+	public double[][] getRows(int[] rowIndexes) {
 		return null;
 	}
-	
-	
-	
-	public double[][] getColumns(int startIndex, int endIndex){
-			return null;
-	}
-	
-	public double[][] getColumns(String [] columnNames){
+
+	/***
+	 * returns multiple columns
+	 * 
+	 * @param startIndex
+	 *            first index of columns
+	 * @param endIndex
+	 *            last index of columns
+	 * @return the columns that we're returning
+	 */
+	public double[][] getColumns(int startIndex, int endIndex) {
 		return null;
-}
-	
-	public void setIndividualEntry(int rowIndex, int columnIndex, double newValue){
-		
 	}
-	
-	public void saveToFile(String filename){
-		
+
+	public double[][] getColumns(String[] columnNames) {
+		return null;
 	}
-	
-	
-	
-	
+
+	/***
+	 * 
+	 * @param rowIndex index of the row to return
+	 * @param columnIndex index of the column to return
+	 * @param newValue new value to set existing entry to
+	 * @ return new Individual entry
+	 */
+	public void setIndividualEntry(int rowIndex, int columnIndex, double newValue) {
+
+	}
+
+	public void saveToFile(String filename) {
+
+	}
+
 }
